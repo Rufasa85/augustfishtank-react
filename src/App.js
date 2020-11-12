7f2234576054e08db1e33d49cb9869acd561b02a
Joe Rehfuss <joe.rehfuss@gmail.com>
2020-11-12 12:03:00 -0800

    front end stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from "react"
+import API from "./utils/API"
 
 function App() {
+  const [loginFormState, setLoginFormState] = useState({
+    email:"",
+    password:""
+  })
+
+  const [profileState,setProfileState]= useState({
+    name:"",
+    email:"",
+    tanks:[],
+    isLoggedIn:false
+  })
+
+  useEffect(()=>{
+    const token = localStorage.getItem("token");
+    API.getProfile(token).then(profileData=>{
+      if(profileData){
+        setProfileState({
+          name:profileData.name,
+          email:profileData.email,
+          tanks:profileData.Tanks,
+          isLoggedIn:true
+        })
+      }
+    }
+    )
+  },[])
+
+  const inputChange = event=>{
+    const {name,value}=event.target;
+    setLoginFormState({
+      ...loginFormState,
+      [name]:value
+    })
+  }
+
+  const formSubmit = event=>{
+    event.preventDefault();
+    API.login(loginFormState).then(newToken=>{
+      localStorage.setItem("token",newToken.token)
+      API.getProfile(newToken.token).then(profileData=>{
+       setProfileState({
+         name:profileData.name,
+         email:profileData.email,
+         tanks:profileData.Tanks,
+         isLoggedIn:true
+       })
+      })
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={formSubmit}>
+        <input onChange = {inputChange} value={loginFormState.email} type='text' name="email" placeholder="email"/>
+        <input onChange = {inputChange} value={loginFormState.password} type='password' name="password" />
+        <input type="submit" value="login"/>
+      </form>
+      {profileState.isLoggedIn?profileState.tanks.map(tankObj=><p>{tankObj.name}</p>):<h1>Login to see your tanks</h1>}
     </div>
   );
 }
